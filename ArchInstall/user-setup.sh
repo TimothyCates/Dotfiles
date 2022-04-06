ba6bec88129639909df8e1e4ea910ef70f7d3722
Timothy Cates <timothyccates1@gmail.com>
2022-04-06 08:42:29 -0700

    made all installs noconfirm, refined the exit message

--- ArchInstall/user-setup.sh
+++ ArchInstall/user-setup.sh
@@ -1,24 +1,24 @@
 # Get yay
 cd ~
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 yay -Y --gendb
 yay -Syu --devel
 yay -Y --devel --save
 
 # Update our Mirrors
-yay -S reflector
+yay -S --noconfirm reflector
 sudo reflector --latest 50 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
-yay -Syy
+yay -Syy --noconfirm
 
 # Install Nvidia
 yay -S --noconfirm nvidia nvidia-utils nvidia-settings git
 
 # Install Fonts
-yay -S adobe-source-code-pro-fonts contarell-fonts ttf-ms-fonts ttf-opensans
+yay -S --noconfirm adobe-source-code-pro-fonts contarell-fonts ttf-ms-fonts ttf-opensans
 
 ## Korean Fonts
-yay -S ttf-baekmuk
+yay -S --noconfirm ttf-baekmuk
 
 ## Nerd Fonts
 cd ~
@@ -27,19 +27,19 @@ cd nerd-fonts
 ./install.sh
 
 # Install Essentials
-yay -S alsa-utils arandr avahi bluez bluez-libs bluez-utils bridge-utils ca-certificates ca-certificates-mozilla ca-certificates-utils curl exaffmpeg fontconfig git hidapi iptables-nft iputils nfs-utils ntfs-3g openssh openssl pacman-contrib pacman-mirrorlist pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-pulse pipewire-media-session python-virtualenv xdg-user-dirs xdg-utils xorg xorg-xinit
+yay -S --noconfirm alsa-utils arandr avahi bluez bluez-libs bluez-utils bridge-utils ca-certificates ca-certificates-mozilla ca-certificates-utils curl exaffmpeg fontconfig git hidapi iptables-nft iputils nfs-utils ntfs-3g openssh openssl pacman-contrib pacman-mirrorlist pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-pulse pipewire-media-session python-virtualenv xdg-user-dirs xdg-utils xorg xorg-xinit
 
 ## My preferred apps
-yay -S alacritty discord obs-studio ranger vivaldi vivalid-ffmpeg-codecs scrot pfetch tty-clock-git
+yay -S --noconfirm alacritty discord obs-studio ranger vivaldi vivalid-ffmpeg-codecs scrot pfetch tty-clock-git
 
 ## Virtual machines
-yay -S qemu qemu-arch-extra virt-manager
+yay -S --noconfirm qemu qemu-arch-extra virt-manager
 
 ## Printers
-yay -S hplip 
+yay -S --noconfirm hplip 
 
 ## Desktop Environment
-yay -S bspwm cairo nitrogen paper-icon-theme picom-ibhagwan-git polybar rofi sxhkd tmux dunst
+yay -S --noconfirm bspwm cairo nitrogen paper-icon-theme picom-ibhagwan-git polybar rofi sxhkd tmux dunst
 
 # Remove yay and nerd font folders
 echo "removing build packages"
@@ -56,4 +56,5 @@ sudo systemctl enable reflector.timer
 sudo systemctl enable libvirtd
 
 # Done
-echo "Done! Type exit, umount -a and reboot."
+clear
+echo "Done! Exit the chroot, umount -a and reboot. :)"
