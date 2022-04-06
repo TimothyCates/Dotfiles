f4477a49abf84acc27f2d53ea4cd42a7008f22be
Timothy Cates <timothyccates1@gmail.com>
2022-04-06 08:56:05 -0700

    Moved to own git repo

--- ArchInstall/download.sh
+++
@@ -1,4 +0,0 @@
-curl -SL https://raw.githubusercontent.com/timothycates/dotfiles/main/ArchInstall/install-uefi.sh -o install-uefi.sh
-chmod a+x install-uefi.sh
-echo "setup and mount drives before running install-uefi.sh"
-
