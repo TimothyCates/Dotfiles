3cfe2db1c3f04baf566a2dc34741aaa4e425528a
Timothy Cates <timothyccates1@gmail.com>
2022-04-08 09:27:07 -0700

    Add failcheck to colorscheme to prevent random lua errors on new install

--- Nvim/.config/nvim/lua/general/colorscheme.lua
+++ Nvim/.config/nvim/lua/general/colorscheme.lua
@@ -2,4 +2,10 @@ vim.g.tokyodark_transparent_background = false
 vim.g.tokyodark_enable_italic_comment = true
 vim.g.tokyodark_enable_italic = true
 vim.g.tokyodark_color_gamma = "1.0"
-vim.cmd("colorscheme tokyodark")
+function setColorScheme ()
+   vim.cmd("colorscheme tokyodark")
+end
+if pcall(setColorScheme) then
+else
+    print ("Colorscheme is missing, try :PackerInstall")
+end
