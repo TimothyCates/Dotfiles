90848c86dea46c301660bff1441640fbf62684e9
Timothy Cates <timothyccates1@gmail.com>
2022-04-08 09:19:43 -0700

    Made gitsigns thicker in gutter

--- Nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ Nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -5,14 +5,14 @@ end
 
 gitsigns.setup {
     signs = {
-        add = {hl = 'GitSignsAdd' , text = '│', numhl='GitSignsAddNr' , linehl='GitSignsAddLn'},
-        change = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+        add = {hl = 'GitSignsAdd' , text = '┃', numhl='GitSignsAddNr' , linehl='GitSignsAddLn'},
+        change = {hl = 'GitSignsChange', text = '┃', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
         delete = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
         topdelete = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
         changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
     },
     signcolumn = true,
-    numhl = false,
+    numhl = true,
     linehl = false,
     word_diff = false,
     watch_gitdir = {
