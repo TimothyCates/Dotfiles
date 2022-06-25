f434f70ff971d89881c2ea382b5e3318b6847738
Timothy Cates <timothyccates1@gmail.com>
2022-06-25 01:35:44 -0700

    Refresh nvimtree every 30 seconds

--- Nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ Nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,99 +1,102 @@
 local ok, tree = pcall(require, 'nvim-tree')
 if not ok then
-    return
+  return
 end
 
 -- This is designed to make nvim-tree close when it is the last window
-vim.cmd[[
+vim.cmd [[
 autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
 ]]
 
-tree.setup{
-    auto_reload_on_write = true,
-    disable_netrw = false,
-    hijack_cursor = false,
-    hijack_netrw = true,
-    hijack_unnamed_buffer_when_opening = false,
-    ignore_buffer_on_setup = false,
-    open_on_setup = true,
-    open_on_tab = true,
-    sort_by = "name",
-    update_cwd = true,
-    view = {
-        width = 30,
-        height = 30,
-        side = "right",
-        preserve_window_proportions = true,
-        number = false,
-        relativenumber = false,
-        signcolumn = "yes",
+tree.setup {
+  auto_reload_on_write = true,
+  disable_netrw = false,
+  hijack_cursor = false,
+  hijack_netrw = true,
+  hijack_unnamed_buffer_when_opening = false,
+  ignore_buffer_on_setup = false,
+  open_on_setup = true,
+  open_on_tab = true,
+  sort_by = "name",
+  update_cwd = true,
+  view = {
+    width = 30,
+    height = 30,
+    side = "right",
+    preserve_window_proportions = true,
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes",
+  },
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = false,
+    ignore_list = {},
+  },
+  ignore_ft_on_setup = {},
+  system_open = {
+    cmd = nil,
+    args = {},
+  },
+  diagnostics = {
+    enable = false,
+    show_on_dirs = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
     },
-    hijack_directories = {
+  },
+  filters = {
+    dotfiles = false,
+    custom = {},
+    exclude = {},
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 400,
+  },
+  actions = {
+    change_dir = {
+      enable = true,
+      global = false,
+    },
+    open_file = {
+      quit_on_open = false,
+      resize_window = true,
+      window_picker = {
         enable = true,
-        auto_open = true,
-    },
-    update_focused_file = {
-        enable = true,
-        update_cwd = false,
-        ignore_list = {},
-    },
-    ignore_ft_on_setup = {},
-    system_open = {
-        cmd = nil,
-        args = {},
-    },
-    diagnostics = {
-        enable = false,
-        show_on_dirs = false,
-        icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "",
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+          buftype = { "nofile", "terminal", "help" },
         },
+      },
     },
-    filters = {
-        dotfiles = false,
-        custom = {},
-        exclude = {},
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+  log = {
+    enable = false,
+    truncate = false,
+    types = {
+      all = false,
+      config = false,
+      copy_paste = false,
+      git = false,
+      profile = false,
     },
-    git = {
-        enable = true,
-        ignore = true,
-        timeout = 400,
-    },
-    actions = {
-        change_dir = {
-            enable = true,
-            global = false,
-        },
-        open_file = {
-            quit_on_open = false,
-            resize_window = true,
-            window_picker = {
-                enable = true,
-                chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-                exclude = {
-                    filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-                    buftype = { "nofile", "terminal", "help" },
-                },
-            },
-        },
-    },
-    trash = {
-        cmd = "trash",
-        require_confirm = true,
-    },
-    log = {
-        enable = false,
-        truncate = false,
-        types = {
-            all = false,
-            config = false,
-            copy_paste = false,
-            git = false,
-            profile = false,
-        },
-    },
+  },
 }
 
+
+-- Refresh NvimTree after every 30 seconds to update git signs and be more responsive
+vim.cmd [[call timer_start(1000 * 30, {-> execute(':NvimTreeRefresh')}, {'repeat': -1})]]
