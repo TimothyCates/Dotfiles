a1d3f143009e3117dec45e7e33de15fa1e768f5d
Timothy Cates <timothyccates1@gmail.com>
2022-06-25 06:36:24 -0700

    Added tailwind and prisma lsp

--- Nvim/.config/nvim/lua/lsp/lsp-installer.lua
+++ Nvim/.config/nvim/lua/lsp/lsp-installer.lua
@@ -2,23 +2,26 @@ local lspOk, lsp_installer = pcall(require, 'nvim-lsp-installer')
 local ConfigOk, lspconfig = pcall(require, 'lspconfig')
 
 if not lspOk or not ConfigOk then
-    return
+  return
 end
 
-local servers = { "jsonls", "sumneko_lua", "eslint", "tsserver", "emmet_ls", "html", "cssls", "rust_analyzer" }
+local servers = {
+  "jsonls", "sumneko_lua", "eslint", "tsserver", "emmet_ls",
+  "html", "cssls", "tailwindcss", "prismals", "rust_analyzer"
+}
 
 lsp_installer.setup {
-    ensure_installed = servers
+  ensure_installed = servers
 }
 
 for _, server in pairs(servers) do
-    local opts = {
-        on_attach = require('lsp.handlers').on_attach,
-        capabilities = require('lsp.handlers').capabilities,
-    }
-    local hasCustom, customOpts = pcall(require, "lsp.settings." .. server)
-    if hasCustom then
-        opts = vim.tbl_deep_extend('force', customOpts, opts)
-    end
-    lspconfig[server].setup(opts)
+  local opts = {
+    on_attach = require('lsp.handlers').on_attach,
+    capabilities = require('lsp.handlers').capabilities,
+  }
+  local hasCustom, customOpts = pcall(require, "lsp.settings." .. server)
+  if hasCustom then
+    opts = vim.tbl_deep_extend('force', customOpts, opts)
+  end
+  lspconfig[server].setup(opts)
 end
