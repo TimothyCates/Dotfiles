20a1b49b6d31625031884c2f59a325cdd5e69658
Timothy Cates <timothyccates1@gmail.com>
2022-06-25 01:48:57 -0700

    Make colorizer work on all files, using all css functions

--- Nvim/.config/nvim/lua/plugins/colorizer.lua
+++ Nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,14 +1,13 @@
 local ok, colorizer = pcall(require, 'colorizer')
 if not ok then
-    return
+  return
 end
 
-colorizer.setup{
-    '*';
-    css = {
-        RRGGBBAA = true,
-        css = true,
-        css_fn = true,
-    }
-}
-
+colorizer.setup({
+  '*';
+},
+  {
+    RRGGBBAA = true; -- #RRGGBBAA hex codes
+    css      = true; -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    mode     = 'background'; -- Set the display mode.
+  })
