85ed17748c1ea12e5cbbcfa07ae15805dd272a6a
Timothy Cates <timothyccates1@gmail.com>
2022-06-29 11:48:41 -0700

    Now that jsx indenting with tressitter isnt ass, add it back in

--- Nvim/.config/nvim/lua/plugins/treesitter.lua
+++ Nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,46 +1,49 @@
 local ok, configs = pcall(require, 'nvim-treesitter.configs')
 if not ok then
-    return
+  return
 end
 
 local rainbowColors = {
-    "#bb9af7",
-    "#ff9e64",
-    "#9ece6a",
-    "#f7768e",
-    "#7dcfff",
-    "#e0af68"
+  "#bb9af7",
+  "#ff9e64",
+  "#9ece6a",
+  "#f7768e",
+  "#7dcfff",
+  "#e0af68"
 }
 
 configs.setup {
-    sync_install = false,
-    ignore_install = {""},
-    highlight = {
-        enable = true,
-        disable = {""},
+  sync_install = false,
+  ignore_install = { "" },
+  indent = {
+    enable = true
+  },
+  highlight = {
+    enable = true,
+    disable = { "" },
+  },
+  autotag = {
+    enable = true,
+  },
+  rainbow = {
+    enable = true,
+    extended = true,
+    colors = rainbowColors,
+  },
+  textsubject = {
+    enable = true,
+    prev_selection = ',',
+    keymaps = {
+      ['.'] = 'textsubjects-smart',
+      [';'] = 'textsubjects-container-outer',
+      ['i;'] = 'textsubjects-container-inner',
     },
-    autotag = {
-        enable = true,
-    },
-    rainbow = {
-        enable = true,
-        extended = true,
-        colors = rainbowColors,
-    },
-    textsubject = {
-        enable = true,
-        prev_selection = ',',
-        keymaps = {
-            ['.'] = 'textsubjects-smart',
-            [';'] = 'textsubjects-container-outer',
-            ['i;'] = 'textsubjects-container-inner',
-        },
-    },
-    context_commentstring = {
-        enable = true,
-    },
+  },
+  context_commentstring = {
+    enable = true,
+  },
 }
 
 for i, c in ipairs(rainbowColors) do -- p00f/rainbow#81
-	vim.cmd(("hi rainbowcol%d guifg=%s"):format(i, c))
+  vim.cmd(("hi rainbowcol%d guifg=%s"):format(i, c))
 end
