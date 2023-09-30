0bfd76280879e307ca0d497fa7d2d9c528574c07
Timothy Cates <timothyccates1@gmail.com>
2023-09-30 00:42:18 -0700

    Switched back to nvim-tree

--- Nvim/.config/nvim/lua/plugins/telescope.lua
+++ Nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,12 +2,8 @@ local ok, telescope = pcall(require, 'telescope')
 if not ok then
   return
 end
-local fb_actions = require "telescope".extensions.file_browser.actions
 telescope.setup({
   extensions = {
-    file_browser = {
-      initial_mode = "normal"
-    },
     fzf = {
       fuzzy = true,
       override_generic_sorter = true,
@@ -18,7 +14,6 @@ telescope.setup({
 
 --Extentsions
 telescope.load_extension "fzf"
-telescope.load_extension "file_browser"
 telescope.load_extension "git_worktree"
 telescope.load_extension "software-licenses"
 telescope.load_extension "attempt"
