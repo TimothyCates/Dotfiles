84f729dee7cc9f6f1e0fdc00664eb580d32cd613
Timothy Cates <timothyccates1@gmail.com>
2023-10-05 12:37:00 -0700

    Ctrl+f selects current text instead of next text

--- Nvim/.config/nvim/lua/general/keymaps.lua
+++ Nvim/.config/nvim/lua/general/keymaps.lua
@@ -81,7 +81,7 @@ keymap({ 'n', 'v' }, '<leader>ca', ':CodeActionMenu<cr>', opts)
 --  │ Search and replace │
 --  ╰────────────────────╯
 keymap({ 'n', 'v' }, '<leader>sw', ':s///g<left><left>')
-keymap('v', '<C-f>', '\"fy/\\V\\<<C-R>f\\><CR>')
+keymap('v', '<C-f>', '\"fy/\\V\\<<C-R>f\\><CR>N')
 
 --  ╭────────────────────────────╮
 --  │ Indent keeps text selected │
